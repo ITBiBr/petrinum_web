2770683017bd9e54a440ad9e42f0aa932eade0f6
Štrobl Luděk <strobl@biskupstvi.cz>
2026-04-16 10:38:19 +0200

    uvod controller nacita clanek uvod

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,21 +2,23 @@
 
 namespace App\Controller;
 
+use App\Repository\ClankyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(): Response
+    public function index(ClankyRepository $clankyRepository): Response
     {
-        $obsah = [];
-        for ($i = 0; $i < 100; $i++) {
-            $obsah[$i] = random_int(1, 100);
-        }
-        return $this->render('main/index.html.twig', [
-            'obsah' => $obsah,
+        $clanek = $clankyRepository->findOneBy(['url' => 'uvod']);
+        if (!$clanek)
+            return new Response('Článek "úvod" byl pravděpodobně odstraněn.');
+        return $this->render('clanky/clanek.html.twig', [
+            'controller_name' => 'AkceController',
+            'akce' => $clanek,
         ]);
     }
 }
